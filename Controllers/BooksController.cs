11031cb2e7069f13b40379236d6b90f318ffa1e7
Keaton Wineman <Keatonwineman@gmail.com>
2018-02-12 23:27:34 -0500

    Bonus lab step 1

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Fisher.Bookstore.Models;
 
 namespace Fisher.Bookstore.Controllers
 {
@@ -11,11 +12,30 @@ namespace Fisher.Bookstore.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            var books = GetBooks();
+            return View(books);
         }
         public IActionResult New()
         {
             return View();
+        }
+        private IEnumerable<Book> GetBooks()
+        {
+            var books = new List<Book>();
+
+            books.Add(new Book(){
+                Title = "Continuous Delivery",
+                Author = new Author(){Name = "Jez Humble"}});
+
+            books.Add(new Book()
+            {
+                Title = "Lean Enterprise",
+                Author = new Author(){Name = "Barry O'Reilly"}});
+            books.Add(new Book()
+            {
+                Title = "The Goal",
+                Author = new Author(){Name = "Eliyahu Goldratt"}});
+            return books;
         }
     }
 }
