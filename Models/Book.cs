bce4be8f9ae8b869ba64071e7c2c5900f4bd3258
Keaton Wineman <Keatonwineman@gmail.com>
2018-02-12 23:49:21 -0500

    Added razor-model

---
+++ Models/Book.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Fisher.Bookstore.Models
+{
+    public class Book
+    {
+        public int BookId{ get; set;}
+        public String Title{ get; set;}
+        public Author Author{get; set;}
+        public void SellBook()
+        {
+            //code to sell book goes here
+        }
+    }
+}
